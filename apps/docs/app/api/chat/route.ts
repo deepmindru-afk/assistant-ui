d49032007294956e6c8213bba2d8a2078388bc13
deepmindru-afk <deepmindru@gmail.com>
2025-09-19 23:56:43 +0300

    Update route.ts

--- apps/docs/app/api/chat/route.ts
+++ apps/docs/app/api/chat/route.ts
@@ -3,6 +3,7 @@ import { convertToModelMessages, stepCountIs, streamText } from "ai";
 //import { kv } from "@vercel/kv";
 //import { Ratelimit } from "@upstash/ratelimit";
 import { frontendTools } from "@assistant-ui/react-ai-sdk";
+import { xai } from '@ai-sdk/xai';
 
 export const maxDuration = 30;
 
@@ -23,13 +24,12 @@ export async function POST(req: Request) {
   const { messages, tools } = await req.json();
   //const ip = req.headers.get("x-forwarded-for") ?? "ip";
   //const { success } = await ratelimit.limit(ip);
-
   //if (!success) {
   //  return new Response("Rate limit exceeded", { status: 429 });
   //}
 
   const result = streamText({
-    model: openai("gpt-4o-mini"),
+    model: xai('grok-2-vision-1212'), //openai("mistral/mistral-large-latest"),
     messages: convertToModelMessages(messages),
     maxOutputTokens: 1200,
     stopWhen: stepCountIs(10),
